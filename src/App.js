fe65b16aab54ae9572a31c8aaa10477e419da1b3
unnamed <>
2019-08-16 14:10:09 +0200

    basic folders structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import classes from './App.module.css';
 
 function App() {
+
+  const [title, setDocumentTitle] = useState('sszat');
+
+  useEffect(() => {
+    const _title = `sszat -- ${new Date()}`;
+    setDocumentTitle(_title);
+    document.title = _title;
+  }, [title]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      {title}
     </div>
   );
 }
