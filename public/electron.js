0dbf79b2b1a4cf30d9d540c44512128dbaea3f7a
unnamed <>
2019-08-23 15:14:46 +0200

    console.logs for debbugging

--- public/electron.js
+++ public/electron.js
@@ -1,22 +1,27 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-
+console.log(1);
 const path = require('path');
 const isDev = require('electron-is-dev');
 
 let mainWindow;
+console.log(2);
+
 
 function createWindow() {
+console.log(4);
+
     mainWindow = new BrowserWindow({width: 800, height: 450});
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 
+    console.log('createWindow - isDev ',isDev);
     if (isDev) {
         // Open DevTools.
         BrowserWindow.addDevToolsExtension('C:/Users/kteresz/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.0.5_0');
         mainWindow.webContents.openDevTools();
     }
-    mainWindow.on('closed', () => mainWindow = null);
+    mainWindow.on('closed', () => {console.log('closed');mainWindow = null;});
 }
 
 app.on('ready', createWindow);
@@ -24,11 +29,14 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
+        console.log('app.quit()');
     }
 });
 
 app.on('activate', () => {
     if (mainWindow === null) {
+        console.log('activated - createWindow()');
         createWindow();
     }
 });
+console.log(3);
