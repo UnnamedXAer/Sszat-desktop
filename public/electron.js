e6a52fcdf3b539e82f99995bd49221859f2b32c4
unnamed <>
2019-08-26 09:32:32 +0200

    trying to fix issue with not showing window

--- public/electron.js
+++ public/electron.js
@@ -12,33 +12,35 @@ console.log(2);
 
 
 function createWindow() {
-console.log(4);
+    console.log(4);
 
-    mainWindow = new BrowserWindow({width: 800, height: 450});
+    mainWindow = new BrowserWindow({ width: 800, height: 450 });
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 
-    console.log('createWindow - isDev ',isDev);
+    console.log('createWindow - isDev ', isDev);
     if (isDev) {
         // Open DevTools.
         BrowserWindow.addDevToolsExtension(process.env.REACT_DEV_TOOLS_PATH);
         mainWindow.webContents.openDevTools();
     }
-    mainWindow.on('closed', () => {console.log('closed');mainWindow = null;});
+    
+    mainWindow.on('closed', function () {
+        console.log(7);
+        mainWindow = null;
+    });
 }
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-        console.log('app.quit()');
-    }
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+    console.log(6);
 });
 
-app.on('activate', () => {
-    if (mainWindow === null) {
-        console.log('activated - createWindow()');
-        createWindow();
-    }
+app.on('activate', function () {
+    if (mainWindow === null) createWindow();
+    console.log(5);
 });
+
 console.log(3);
