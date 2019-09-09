089e5181fa176390baf6df05a8ff4fcb30f15ec9
unnamed <>
2019-09-09 14:57:11 +0200

    created mask component

---
+++ src/components/UI/Mask/Mask.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from 'Mask.module.css';
+
+function mask(props) {
+    return (
+        <div className={classes.Mask}>
+            
+        </div>
+    );
+};
+
+export default mask;
