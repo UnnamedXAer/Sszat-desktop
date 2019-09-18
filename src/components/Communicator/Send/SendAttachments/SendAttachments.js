289eca2cb20ae4c70bb51757d3f43341bb98071d
unnamed <>
2019-09-18 15:20:52 +0200

    installed react-tooltip-light

--- src/components/Communicator/Send/SendAttachments/SendAttachments.js
+++ src/components/Communicator/Send/SendAttachments/SendAttachments.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import classes from './SendAttachments.module.css';
 import Attachment from './Attachment/Attachment';
+import Tooltip from 'react-tooltip-lite';
 
 const sendAttachments = React.memo(props => {
 
-    const attachments = props.files.map(x => <Attachment key={x.path} file={x.data} ext={x.ext} path={x.path} deleteAttachment={props.deleteAttachment} />);
-
+    const attachments = props.files.map(x => (
+        <Tooltip 
+            key={x.path} 
+            content={x.path}
+            background={getComputedStyle(document.documentElement).getPropertyValue('--color-bg-hover-aside')}
+            color={getComputedStyle(document.documentElement).getPropertyValue('--color-font')}
+        >
+            <Attachment file={x.data} ext={x.ext} path={x.path} deleteAttachment={props.deleteAttachment} />
+        </Tooltip>
+    ));
 
     return (
         <div className={classes.SendAttachments} >
