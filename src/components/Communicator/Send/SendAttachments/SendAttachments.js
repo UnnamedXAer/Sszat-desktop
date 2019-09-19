d96bd93ade671c2f059aea29ef45718f182135d2
unnamed <>
2019-09-19 09:02:18 +0200

    scrolling send attachments.

--- src/components/Communicator/Send/SendAttachments/SendAttachments.js
+++ src/components/Communicator/Send/SendAttachments/SendAttachments.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import classes from './SendAttachments.module.css';
 import Attachment from './Attachment/Attachment';
 import Tooltip from 'react-tooltip-lite';
 
 const sendAttachments = React.memo(props => {
+
+    const attachmentsContainerRef = useRef();
 
     const attachments = props.files.map(x => (
         <Tooltip 
@@ -16,9 +18,24 @@ const sendAttachments = React.memo(props => {
         </Tooltip>
     ));
 
+    const wheelHandler = (ev) => {
+        var delta = - 20 * (Math.max(-1, Math.min(1, (ev.nativeEvent.wheelDelta || -ev.nativeEvent.detail))));
+    
+        var pst = attachmentsContainerRef.current.scrollLeft + delta;
+        if (pst < 0) {
+            pst = 0;
+        } else if (pst > attachmentsContainerRef.current.scrollWidth) {
+            pst = attachmentsContainerRef.current.scrollWidth;
+        }
+    
+        attachmentsContainerRef.current.scrollLeft = pst;
+    
+        return false;
+    }
+
     return (
         <div className={classes.SendAttachments} >
-            <div className={classes.SendAttachmentsContainer} >
+            <div className={classes.SendAttachmentsContainer} onWheel={wheelHandler} ref={attachmentsContainerRef} >
                 {attachments}
                 {attachments.length === 0 && props.draggedOverApp && <Attachment />}
             </div>
