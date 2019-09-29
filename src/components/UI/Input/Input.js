6598f4e551e390991d59febab9614da4c16ff3f0
unknown <rkl2@o2.pl>
2019-09-29 15:09:20 +0200

    input error border added

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,8 +2,39 @@ import React from 'react';
 import classes from './Input.module.css';
 
 const input = (props) => {
+    const {
+        value,
+        required,
+        disabled,
+        readOnly,
+        onChange,
+        onBlur,
+        onFocus,
+        placeholder,
+        name,
+        type,
+
+        validationError
+    } = props;
+    
+    const styles = [classes.Input];
+    if (validationError) {
+        styles.push(classes.Error);
+    }
+
     return (
-        <input className={classes.Input} {...props} />
+        <input className={styles.join(" ")} 
+            value={value}
+            required={required}
+            disabled={disabled}
+            readOnly={readOnly}
+            onChange={onChange}
+            onBlur={onBlur}
+            onFocus={onFocus}
+            placeholder={placeholder}
+            name={name}
+            type={type}
+         />
     );
 };
 
