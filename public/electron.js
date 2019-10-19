48b61c2c6911b1e1f9df640ad3ce766d3430041e
unknown <rkl2@o2.pl>
2019-10-19 12:42:44 +0200

    added debug package

--- public/electron.js
+++ public/electron.js
@@ -6,54 +6,59 @@ const BrowserWindow = electron.BrowserWindow;
 const fs = require('fs');
 const path = require('path');
 const isDev = require('electron-is-dev');
-const dl = require('../src/utils/downloadLocation');
+const debug = require("debug")("electron");
+
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 900, 
-    minWidth: 595, /* min width required when send options are expanded */
-    height: 500,
-    minHeight: 280+(20/* because of chromium menu height */),
-    useContentSize: true,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  if (isDev) {
-    // Open the DevTools.
-    BrowserWindow.addDevToolsExtension(process.env.REACT_DEV_TOOLS_PATH);
-    mainWindow.webContents.openDevTools();
-  }
-  ipcMain.on("download-attachment", (ev, payload) => {
-    console.log("payload", payload);
-    const writeStream = new fs.WriteStream(`${dl}/${payload.file.name}`)
-    writeStream.on("finish", (res) => {
-      console.log('saving fishished', res);
-    });
-    writeStream.on("err", (err) => {
-      console.log("file saving error", err);
-    });
+	mainWindow = new BrowserWindow({
+		width: 900,
+		minWidth: 595, /* min width required when send options are expanded */
+		height: 500,
+		minHeight: 280 + (20/* because of chromium menu height */),
+		useContentSize: true,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
 
-    writeStream.write(payload.file.data);
-    writeStream.end();
-  });
+	mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+	if (isDev) {
+		// Open the DevTools.
+		BrowserWindow.addDevToolsExtension(process.env.REACT_DEV_TOOLS_PATH);
+		mainWindow.webContents.openDevTools();
+	}
 
-  mainWindow.on('closed', () => mainWindow = null);
+	ipcMain.on("download-attachment", (ev, payload) => {
+		debug("About to save file. The payload is %O", payload);
+		const dl = require('../src/utils/downloadLocation');
+
+		const writeStream = new fs.WriteStream(`${dl}/${payload.file.name}`)
+		writeStream.on("finish", (res) => {
+			console.log('saving fishished', res);
+		});
+		writeStream.on("err", (err) => {
+			console.log("file saving error", err);
+		});
+
+		writeStream.write(payload.file.data);
+		writeStream.end();
+	});
+
+	mainWindow.on('closed', () => mainWindow = null);
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
