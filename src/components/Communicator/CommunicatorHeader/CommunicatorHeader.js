c3461aa77b6aae8bf3e2ae9656b5bbe7b237bb46
unnamed <>
2019-10-25 08:29:56 +0200

    added logo component

--- src/components/Communicator/CommunicatorHeader/CommunicatorHeader.js
+++ src/components/Communicator/CommunicatorHeader/CommunicatorHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './CommunicatorHeader.module.css';
+import Logo from '../../UI/Logo/Logo';
 
 const communicatorHeader = props => {
     return (
         <div className={classes.CommunicatorHeader} >
             <div className={classes.HeightKeeper}>
+                <Logo />
                 <h2>{props.title}</h2>
             </div>
         </div>
